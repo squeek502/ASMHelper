6aea49eb70830a6ee8edb2f1347399a1db39ee8a
squeek <squeek502@hotmail.com>
2016-01-16 19:14:53 -0800

    Make skipping COMPUTE_FRAMES default to fix getCommonSuperClass errors  - This seems to be what MinecraftForge does with all of its transformers, and it allows us to use the default ClassWriter instead of ObfRemappingClassWriter when running after the FML deobf remapper.
    (cherry picked from commit 05c5e8ded99551f75ad9806fc335c7a59ed809f6)

--- raw/squeek/asmhelper/ASMHelper.java
+++ raw/squeek/asmhelper/ASMHelper.java
@@ -117,7 +117,7 @@ public class ASMHelper
 	 */
 	public static byte[] writeClassToBytes(ClassNode classNode)
 	{
-		return writeClassToBytes(classNode, ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
+		return writeClassToBytes(classNode, ClassWriter.COMPUTE_MAXS);
 	}
 
 	/**
@@ -141,7 +141,7 @@ public class ASMHelper
 	 */
 	public static byte[] writeClassToBytesNoDeobf(ClassNode classNode)
 	{
-		return writeClassToBytesNoDeobf(classNode, ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
+		return writeClassToBytesNoDeobf(classNode, ClassWriter.COMPUTE_MAXS);
 	}
 
 	/**
@@ -150,18 +150,6 @@ public class ASMHelper
 	public static byte[] writeClassToBytesNoDeobf(ClassNode classNode, int flags)
 	{
 		ClassWriter writer = new ClassWriter(flags);
-		classNode.accept(writer);
-		return writer.toByteArray();
-	}
-
-	/**
-	 * Convert a ClassNode into a byte array.
-	 * Does not re-compute frames. Primarily useful for avoiding ClassNotFoundExceptions in
-	 * getCommonSuperClass that aren't solved by {@link #writeClassToBytes}/{@link ObfRemappingClassWriter}.
-	 */
-	public static byte[] writeClassToBytesNoDeobfSkipFrames(ClassNode classNode)
-	{
-		ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS);
 		classNode.accept(writer);
 		return writer.toByteArray();
 	}
