15123cea1c357f1568b654953af8ba7cff4c9b5f
Ryan Liptak <squeek502@hotmail.com>
2019-12-13 01:10:38 -0800

    Port findReturnsNullWhenNeedleIsNotFoundOrEmpty test

--- src/test/java/squeek/asmhelper/TestASMHelper.java
+++ src/test/java/squeek/asmhelper/TestASMHelper.java
@@ -55,23 +55,13 @@ public class TestASMHelper
 	{
 		CoreModTestRunner.runJavascriptTest("findReturnsTheStartOfTheNeedleFoundInTheHaystack.js");
 	}
-/*
+
 	@Test
-	public void findReturnsNullWhenNeedleIsNotFoundOrEmpty()
+	void findReturnsNullWhenNeedleIsNotFoundOrEmpty()
 	{
-		InsnList needle = new InsnList();
-		InsnList haystack = populateTestHaystack(new InsnList());
-
-		assertNull(ASMHelper.find(haystack, needle));
-
-		needle.add(new VarInsnNode(ALOAD, 0));
-		needle.add(new FieldInsnNode(GETFIELD, InsnComparator.WILDCARD, "foodLevel", "I"));
-		needle.add(new VarInsnNode(ISTORE, 3));
-		needle.add(new VarInsnNode(ALOAD, 0));
-
-		assertNull(ASMHelper.find(haystack.get(3), needle));
+		CoreModTestRunner.runJavascriptTest("findReturnsNullWhenNeedleIsNotFoundOrEmpty.js");
 	}
-
+/*
 	@Test
 	public void findAndReplaceReturnsTheInstructionAfterTheReplacement()
 	{
@@ -181,80 +171,6 @@ public class TestASMHelper
 		// inside the needle are included in the replacement
 		ASMHelper.findAndReplace(haystack, needle, replacement);
 		assertEquals(3, haystack.size());
-	}
-
-	public InsnList populateTestHaystack(InsnList haystack)
-	{
-		haystack.clear();
-		LabelNode l0 = new LabelNode();
-		haystack.add(l0);
-		haystack.add(new LineNumberNode(44, l0));
-		haystack.add(new VarInsnNode(ALOAD, 0));
-		haystack.add(new FieldInsnNode(GETFIELD, "net/minecraft/util/FoodStats", "foodLevel", "I"));
-		haystack.add(new VarInsnNode(ISTORE, 3));
-		LabelNode l1 = new LabelNode();
-		haystack.add(l1);
-		haystack.add(new LineNumberNode(45, l1));
-		haystack.add(new VarInsnNode(ALOAD, 0));
-		haystack.add(new FieldInsnNode(GETFIELD, "net/minecraft/util/FoodStats", "entityplayer", "Lnet/minecraft/entity/player/EntityPlayer;"));
-		haystack.add(new VarInsnNode(ALOAD, 1));
-		haystack.add(new VarInsnNode(ALOAD, 2));
-		haystack.add(new MethodInsnNode(INVOKEVIRTUAL, "net/minecraft/item/ItemFood", "func_150905_g", "(Lnet/minecraft/item/ItemStack;)I", false));
-		haystack.add(new VarInsnNode(ILOAD, 3));
-		haystack.add(new InsnNode(IADD));
-		haystack.add(new MethodInsnNode(INVOKESTATIC, "org/bukkit/craftbukkit/event/CraftEventFactory", "callFoodLevelChangeEvent", "(Lnet/minecraft/entity/player/EntityPlayer;I)Lorg/bukkit/event/entity/FoodLevelChangeEvent;", false));
-		haystack.add(new VarInsnNode(ASTORE, 4));
-		LabelNode l2 = new LabelNode();
-		haystack.add(l2);
-		haystack.add(new LineNumberNode(47, l2));
-		haystack.add(new VarInsnNode(ALOAD, 4));
-		haystack.add(new MethodInsnNode(INVOKEVIRTUAL, "org/bukkit/event/entity/FoodLevelChangeEvent", "isCancelled", "()Z", false));
-		LabelNode l3 = new LabelNode();
-		haystack.add(new JumpInsnNode(IFNE, l3));
-		LabelNode l4 = new LabelNode();
-		haystack.add(l4);
-		haystack.add(new LineNumberNode(49, l4));
-		haystack.add(new VarInsnNode(ALOAD, 0));
-		haystack.add(new VarInsnNode(ALOAD, 4));
-		haystack.add(new MethodInsnNode(INVOKEVIRTUAL, "org/bukkit/event/entity/FoodLevelChangeEvent", "getFoodLevel", "()I", false));
-		haystack.add(new VarInsnNode(ILOAD, 3));
-		haystack.add(new InsnNode(ISUB));
-		haystack.add(new VarInsnNode(ALOAD, 1));
-		haystack.add(new VarInsnNode(ALOAD, 2));
-		haystack.add(new MethodInsnNode(INVOKEVIRTUAL, "net/minecraft/item/ItemFood", "func_150906_h", "(Lnet/minecraft/item/ItemStack;)F", false));
-		haystack.add(new MethodInsnNode(INVOKEVIRTUAL, "net/minecraft/util/FoodStats", "addStats", "(IF)V", false));
-		haystack.add(l3);
-		haystack.add(new LineNumberNode(52, l3));
-		haystack.add(new FrameNode(Opcodes.F_APPEND, 2, new Object[]{Opcodes.INTEGER, "org/bukkit/event/entity/FoodLevelChangeEvent"}, 0, null));
-		haystack.add(new VarInsnNode(ALOAD, 0));
-		haystack.add(new FieldInsnNode(GETFIELD, "net/minecraft/util/FoodStats", "entityplayer", "Lnet/minecraft/entity/player/EntityPlayer;"));
-		haystack.add(new TypeInsnNode(CHECKCAST, "net/minecraft/entity/player/EntityPlayerMP"));
-		haystack.add(new FieldInsnNode(GETFIELD, "net/minecraft/entity/player/EntityPlayerMP", "playerNetServerHandler", "Lnet/minecraft/network/NetHandlerPlayServer;"));
-		haystack.add(new TypeInsnNode(NEW, "net/minecraft/network/play/server/S06PacketUpdateHealth"));
-		haystack.add(new InsnNode(DUP));
-		haystack.add(new VarInsnNode(ALOAD, 0));
-		haystack.add(new FieldInsnNode(GETFIELD, "net/minecraft/util/FoodStats", "entityplayer", "Lnet/minecraft/entity/player/EntityPlayer;"));
-		haystack.add(new TypeInsnNode(CHECKCAST, "net/minecraft/entity/player/EntityPlayerMP"));
-		haystack.add(new MethodInsnNode(INVOKEVIRTUAL, "net/minecraft/entity/player/EntityPlayerMP", "getBukkitEntity", "()Lorg/bukkit/craftbukkit/entity/CraftPlayer;", false));
-		haystack.add(new MethodInsnNode(INVOKEVIRTUAL, "org/bukkit/craftbukkit/entity/CraftPlayer", "getScaledHealth", "()F", false));
-		haystack.add(new VarInsnNode(ALOAD, 0));
-		haystack.add(new FieldInsnNode(GETFIELD, "net/minecraft/util/FoodStats", "entityplayer", "Lnet/minecraft/entity/player/EntityPlayer;"));
-		haystack.add(new MethodInsnNode(INVOKEVIRTUAL, "net/minecraft/entity/player/EntityPlayer", "getFoodStats", "()Lnet/minecraft/util/FoodStats;", false));
-		haystack.add(new FieldInsnNode(GETFIELD, "net/minecraft/util/FoodStats", "foodLevel", "I"));
-		haystack.add(new VarInsnNode(ALOAD, 0));
-		haystack.add(new FieldInsnNode(GETFIELD, "net/minecraft/util/FoodStats", "entityplayer", "Lnet/minecraft/entity/player/EntityPlayer;"));
-		haystack.add(new MethodInsnNode(INVOKEVIRTUAL, "net/minecraft/entity/player/EntityPlayer", "getFoodStats", "()Lnet/minecraft/util/FoodStats;", false));
-		haystack.add(new FieldInsnNode(GETFIELD, "net/minecraft/util/FoodStats", "foodSaturationLevel", "F"));
-		haystack.add(new MethodInsnNode(INVOKESPECIAL, "net/minecraft/network/play/server/S06PacketUpdateHealth", "<init>", "(FIF)V", false));
-		haystack.add(new MethodInsnNode(INVOKEVIRTUAL, "net/minecraft/network/NetHandlerPlayServer", "sendPacket", "(Lnet/minecraft/network/Packet;)V", false));
-		LabelNode l5 = new LabelNode();
-		haystack.add(l5);
-		haystack.add(new LineNumberNode(54, l5));
-		haystack.add(new InsnNode(RETURN));
-		LabelNode l6 = new LabelNode();
-		haystack.add(l6);
-
-		return haystack;
 	}
 	*/
 }
---
+++ src/test/javascript/findReturnsNullWhenNeedleIsNotFoundOrEmpty.js
@@ -0,0 +1,27 @@
+var ASMAPI = Java.type("net.minecraftforge.coremod.api.ASMAPI");
+var InsnList = Java.type("org.objectweb.asm.tree.InsnList");
+var Opcodes = Java.type("org.objectweb.asm.Opcodes");
+var VarInsnNode = Java.type("org.objectweb.asm.tree.VarInsnNode");
+var FieldInsnNode = Java.type("org.objectweb.asm.tree.FieldInsnNode");
+var ALOAD = Opcodes.ALOAD;
+var GETFIELD = Opcodes.GETFIELD;
+var ISTORE = Opcodes.ISTORE;
+
+function initializeCoreMod() {
+	ASMAPI.loadFile('../../main/javascript/asmhelper.js');
+	ASMAPI.loadFile('jankytest.js');
+	ASMAPI.loadFile('setup.js');
+	var InsnComparator = ASMHelper.InsnComparator;
+
+	var needle = new InsnList();
+	var haystack = populateTestHaystack(new InsnList());
+
+	assertEquals(null, ASMHelper.find(haystack, needle));
+
+	needle.add(new VarInsnNode(ALOAD, 0));
+	needle.add(new FieldInsnNode(GETFIELD, InsnComparator.WILDCARD, "foodLevel", "I"));
+	needle.add(new VarInsnNode(ISTORE, 3));
+	needle.add(new VarInsnNode(ALOAD, 0));
+
+	assertEquals(null, ASMHelper.find(haystack.get(3), needle));
+}
